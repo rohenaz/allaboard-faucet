da04a92baaba6a3ad8a28e9d6a7ff45a5f3ddd03
Satchmo <rohenaz@gmail.com>
2019-02-23 23:21:30 -0500

    passthrough functions, firefox fix

--- functions/index.js
+++ functions/index.js
@@ -1,49 +1,66 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
-const rp = require('request-promise');
+const functions = require('firebase-functions')
+const rp = require('request-promise')
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin');
-admin.initializeApp();
+const admin = require('firebase-admin')
+admin.initializeApp()
 
+const allaboardUrl = 'https://api.allaboard.cash/faucet'
 
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
 exports.tap = functions.https.onRequest(async (req, res) => {
-    let headers = req.headers;
-    headers.key = functions.config().allaboard.key;
-    req.headers = headers;
+    let cors = handleCors(req, res)
     try {
-        let response = await rp(req);
-        return res.send(response);
+        let response = await requestWrap(cors.req, '/tap')
+        return cors.res.send(response)
     } catch(error) {
-        console.error(error);
-        return res.status(500).send('Something went wrong while sending to allaboard.');
+        console.error(error)
+        return cors.res.status(500).send('Something went wrong while sending to allaboard.')
     }
-  });
-  
+  })
 
-  // Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
 exports.status = functions.https.onRequest(async (req, res) => {
+    let cors = handleCors(req, res)
+
     try {
-        let response = await rp({
-            method: 'GET',
-            // TODO: Configure the `slack.webhook_url` Google Cloud environment variables.
-            uri: url,
-            header: {
-                key: functions.config().allaboard.key 
-            },
-            body: {
-              text: `<${url}|${commits} new commit${commits > 1 ? 's' : ''}> pushed to <${repo.url}|${repo.full_name}>.`,
-            },
-            json: true,
-          });
-        
-        //postToSlack(req.body.compare, req.body.commits.length, req.body.repository);
-        return res.send(ressponse);
+        let response = await requestWrap(cors.req, '/status')
+        return cors.res.send(response)
     } catch(error) {
-        console.error(error);
-        return res.status(500).send('Something went wrong while posting the message to Slack.');
+        console.error(error)
+        return cors.res.status(500).send('Something went wrong while sending to allaboard.')
     }
-});
+})
+
+const handleCors = (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*')
+
+    if (req.method === 'OPTIONS') {
+      // Send response to OPTIONS requests
+      res.set('Access-Control-Allow-Methods', 'GET')
+      res.set('Access-Control-Allow-Headers', 'Content-Type')
+      res.set('Access-Control-Max-Age', '3600')
+      res.status(204).send('')
+    } else {
+      // Set CORS headers for the main request
+      res.set('Access-Control-Allow-Origin', '*')
+    }
+    return {req: req, res: res}
+}
+
+const requestWrap = (req, path) => {
+    console.log('hitting url:', allaboardUrl + path, 'with method:', req.method, 'and body:', req.body, 'form data?', req.formData)
+    let headers = {}
+    headers.key = functions.config().allaboard.key
+
+    let options = {
+        headers: headers,
+        method: req.method,
+        uri: allaboardUrl + path,
+        json: true
+      }
+      if (req.method === 'POST') {
+        options.form = req.body
+      }
+      console.log('using options', options)
+    return rp(options)
+}
